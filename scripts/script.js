cc6185777e692c15d17c47fd7b8cfdfc8fc97217
Inevdhoven <inevdhoven@gmail.com>
2022-11-03 10:17:03 +0100

    Added JS for animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,5 @@
 // JavaScript Document
 console.log("Howdy!");
+
+// https://greensock.com/morphsvg/ en https://greensock.com/forums/topic/18708-reverse-a-morph-and-then-fluidly-repeat-from-the-start/
+TweenMax.to("#start", 2, {morphSVG: "#eind", ease: Sine.easeInOut, yoyo:true, repeat:-1});
